9fd4537f6e9abb62badb3cde31fba8d0c0273218
Jad Joubran <joubran.jad@gmail.com>
2018-02-05 21:19:02 +0100

    use appRoot module

--- scripts/install.js
+++ scripts/install.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
+const appRoot = require('app-root-path');
 const { COPYFILE_EXCL } = fs.constants;
 
-console.log(`${__dirname }/webdash-config.json`);
-fs.copyFile(`${__dirname}/webdash-config.json`, 'webdash.json', COPYFILE_EXCL, (err) => {
+console.log({appRoot: appRoot.toString()});
+console.log(`${__dirname}/webdash-config.json`);
+fs.copyFile(`${__dirname}/webdash-config.json`, `${appRoot}/webdash.json`, COPYFILE_EXCL, (err) => {
     if (err){
         //configuration file already exists, we can safely proceed
+        return false;
     }
     console.log('Configuration file created at webdash.json');
 });
