4bfba7cd2498719f08fb95dd2971dcef09adb901
Jad Joubran <joubran.jad@gmail.com>
2018-02-05 21:27:50 +0100

    fix polymer.json

--- scripts/install.js
+++ scripts/install.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const appRoot = require('app-root-path');
 const { COPYFILE_EXCL } = fs.constants;
 
-console.log({appRoot: appRoot.toString()});
-console.log(`${__dirname}/webdash-config.json`);
 fs.copyFile(`${__dirname}/webdash-config.json`, `${appRoot}/webdash.json`, COPYFILE_EXCL, (err) => {
     if (err){
         //configuration file already exists, we can safely proceed
