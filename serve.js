061b2cbea66a25e875b8e8b2aaa7c86dcf6106ba
Jad Joubran <joubran.jad@gmail.com>
2018-02-06 16:58:34 +0100

    path

--- serve.js
+++ serve.js
@@ -4,9 +4,9 @@ require('./api.js');
 
 const app = express();
 
-app.use(express.static(__dirname + '/build/es6-bundled'));
+app.use(express.static('./build/es6-bundled'));
 
-app.use('/bower_components', express.static(__dirname + '/bower_components/'));
+app.use('/bower_components', express.static('./bower_components/'));
 
 app.get('/', (req, res) => {
     res.sendFile("build/es6-bundled/index.html", { root: '.' });
