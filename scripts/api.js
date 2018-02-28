ad0f79af87240e91968ef6009b451217026b3265
Jad Joubran <joubran.jad@gmail.com>
2018-02-28 17:28:31 +0100

    Implement new design

--- scripts/api.js
+++ scripts/api.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
-let appRoot = require('app-root-path').toString();
-let npmLinkMode = process.argv.find(arg => arg.startsWith('--project='));
-if (npmLinkMode){
-    appRoot = npmLinkMode.substr('--project='.length);
+let appRoot = require("app-root-path").toString();
+let npmLinkMode = process.argv.find(arg => arg.startsWith("--project="));
+if (npmLinkMode) {
+  appRoot = npmLinkMode.substr("--project=".length);
 }
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 const getConfig = () => {
-    let config = {};
-    if (fs.existsSync(`${appRoot}/webdash.json`)) {
-        config = require(`${appRoot}/webdash.json`);
-    }
-    return config;
-}
+  let config = {};
+  if (fs.existsSync(`${appRoot}/webdash.json`)) {
+    config = require(`${appRoot}/webdash.json`);
+  }
+  return config;
+};
 
 app.locals.config = getConfig();
 
@@ -29,76 +29,78 @@ const bowerJson = require(`${appRoot}/bower.json`);
 const plugins = getPlugins(bowerJson);
 
 for (const plugin of plugins) {
-    const apiPath = `${appRoot}/bower_components/${plugin}/api.js`;
-    if (!fs.existsSync(apiPath)){
-        continue;
+  const apiPath = `${appRoot}/bower_components/${plugin}/api.js`;
+  if (!fs.existsSync(apiPath)) {
+    continue;
+  }
+  const pluginSrc = require(apiPath);
+
+  if (!pluginSrc.routes) {
+    continue;
+  }
+  //remove leading "webdash-"
+  const pluginRoute = plugin.replace(/^webdash\-/, "");
+  const routes = pluginSrc.routes;
+  if (routes.get) {
+    for (const [route, handler] of Object.entries(routes.get)) {
+      app.get(`/${pluginRoute}/${route}`, handler);
     }
-    const pluginSrc = require(apiPath);
-    
-    if (!pluginSrc.routes) {
-        continue;
+  }
+  if (routes.post) {
+    for (const [route, handler] of Object.entries(routes.post)) {
+      app.post(`/${pluginRoute}/${route}`, handler);
     }
-    //remove leading "webdash-"
-    const pluginRoute = plugin.replace(/^webdash\-/, '');
-    const routes = pluginSrc.routes;
-    if (routes.get) {
-        for (const [route, handler] of Object.entries(routes.get)) {
-            app.get(`/${pluginRoute}/${route}`, handler);
-        }
-    }
-    if (routes.post) {
-        for (const [route, handler] of Object.entries(routes.post)) {
-            app.post(`/${pluginRoute}/${route}`, handler);
-        }
-    }
+  }
 }
 
-app.get('/webdash/info', (req, res) => {
-    const packageJson = require(`${appRoot}/package.json`);
-    let appName = packageJson.name;
-    if (!appName){
-        //fallback to folder name
-        appName = appRoot.substr(appRoot.lastIndexOf('/') + 1)
-    }
-    const appPath = appRoot;
+app.get("/webdash/info", (req, res) => {
+  const packageJson = require(`${appRoot}/package.json`);
+  let appName = packageJson.name;
+  if (!appName) {
+    //fallback to folder name
+    appName = appRoot.substr(appRoot.lastIndexOf("/") + 1);
+  }
+  const appPath = appRoot;
+  let version = "0.0.0";
 
-    res.send({ appName, appPath });
+  if (fs.existsSync("webdash/package.json")) {
+    version = require("webdash/package.json").version;
+  }
+
+  res.send({ appName, appPath, version });
 });
 
-app.get('/webdash/config', (req, res) => {
-    let config = {};
-    if (fs.existsSync(`${appRoot}/webdash.json`)){
-        config = require(`${appRoot}/webdash.json`);
-    }
+app.get("/webdash/config", (req, res) => {
+  let config = {};
+  if (fs.existsSync(`${appRoot}/webdash.json`)) {
+    config = require(`${appRoot}/webdash.json`);
+  }
 
-    res.send({ config });
+  res.send({ config });
 });
 
+app.get("/webdash/plugins", (req, res) => {
+  const plugins = getPlugins(bowerJson);
 
-app.get('/webdash/plugins', (req, res) => {
-    const plugins = getPlugins(bowerJson);
-
-    res.send({ plugins });
+  res.send({ plugins });
 });
 
+app.use(express.static(__dirname + "/build/es6-bundled"));
 
-
-app.use(express.static(__dirname + '/build/es6-bundled'));
-
-app.get('*', function (req, res) {
-    res.sendFile("index.html", { root: '.' });
+app.get("*", function(req, res) {
+  res.sendFile("index.html", { root: "." });
 });
 
 function getPlugins(bowerJson) {
-    if (!bowerJson) {
-        return [];
-    }
-    const deps = Object.keys(bowerJson.devDependencies);
-    if (!deps) {
-        return [];
-    }
+  if (!bowerJson) {
+    return [];
+  }
+  const deps = Object.keys(bowerJson.devDependencies);
+  if (!deps) {
+    return [];
+  }
 
-    return deps.filter(dep => dep.startsWith('webdash-'));
+  return deps.filter(dep => dep.startsWith("webdash-"));
 }
 
 module.exports = app;
