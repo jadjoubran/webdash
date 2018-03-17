dacc12a64fde387f575b92e0f08db42fb3b9c146
Jad Joubran <joubran.jad@gmail.com>
2018-03-17 18:55:36 +0200

    Implementation for readableAssets #16

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const fs = require('fs');
 const api = require('./api.js');
 
 const app = express();
@@ -13,6 +13,18 @@ app.use('/api', api);
 app.use(express.static(__dirname + basePath));
 
 app.use('/bower_components', express.static('./bower_components/'));
+
+if (fs.existsSync(`${basePath}/webdash.json`)) {
+    const webdashConfig = require(`${basePath}/webdash.json`);
+    if (webdashConfig && webdashConfig.readableAssets){
+      const readableAssets = webdashConfig.readableAssets;
+      const userPath = Object.keys(readableAssets)[0];
+      const publicPath = Object.values(readableAssets)[0];
+
+      //expose userPath as publicPath
+      app.use(publicPath, express.static(userPath));
+    }
+}
 
 app.get('/', (req, res) => {
     res.sendFile(basePath + "/index.html", { root: __dirname })
