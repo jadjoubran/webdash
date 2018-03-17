7f9dec66b3d624bb2ab64e069dd24a01389c504c
Jad Joubran <joubran.jad@gmail.com>
2018-03-17 18:46:18 +0200

    Remove hackery. Closes #15

--- scripts/serve.js
+++ scripts/serve.js
@@ -12,9 +12,7 @@ app.use('/api', api);
 
 app.use(express.static(__dirname + basePath));
 
-app.use('/', express.static('./bower_components/'));
 app.use('/bower_components', express.static('./bower_components/'));
-app.use('/bower_components/bower_components', express.static('./bower_components/'));
 
 app.get('/', (req, res) => {
     res.sendFile(basePath + "/index.html", { root: __dirname })
