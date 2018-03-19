11f02daf03582a13fb07d7d929958ae51c5fa718
Jad Joubran <joubran.jad@gmail.com>
2018-03-19 09:06:46 +0200

    Fix manifest path and js budget path #8

--- scripts/lib/config-helper.js
+++ scripts/lib/config-helper.js
@@ -37,31 +37,33 @@ const jsFiles = FileHound.create()
   .findSync();
 
 if (jsFiles.length) {
+  let path = jsFiles[0];
+  path = path.replace(`${appRoot}/`, "");
   jsBudgetPath =
-    "./" + jsFiles[0].substr(0, jsFiles[0].lastIndexOf("/")) + "/*.js";
+    "./" + path.substr(0, path.lastIndexOf("/")) + "/*.js";
 }
 
 const manifestFiles = FileHound.create()
   .ignoreHiddenDirectories()
   .ignoreHiddenFiles()
-  .path("./")
+  .path(appRoot)
   .discard(/bower_components|node_modules/)
   .match("manifest.json")
   .depth(3)
   .findSync();
 if (manifestFiles.length) {
-  manifestPath = "./" + manifestFiles[0];
+  manifestPath = "./" + manifestFiles[0].replace(`${appRoot}/`, '');
 } else {
   const webManifestFiles = FileHound.create()
     .ignoreHiddenDirectories()
     .ignoreHiddenFiles()
-    .path("./")
+    .path(appRoot)
     .discard(/bower_components|node_modules/)
     .match("site.webmanifest")
     .depth(3)
     .findSync();
   if (webManifestFiles.length) {
-    manifestPath = "./" + webManifestFiles[0];
+    manifestPath = "./" + webManifestFiles[0].replace(`${appRoot}/`, "");
   }
 }
 
