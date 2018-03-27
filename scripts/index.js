0aefa8fd22ebf4f46daa33f58584481bb54412ec
Jad Joubran <joubran.jad@gmail.com>
2018-03-27 15:29:40 +0300

    Allow binding webdash to hostname. Closes #39

--- scripts/index.js
+++ scripts/index.js
@@ -6,20 +6,25 @@ const serveOptions = yargs => {
   .positional('port', {
     describe: 'port to bind on',
     default: 3456
+  })
+  .positional('host', {
+    describe: 'hostname',
+    default: 'localhost'
   });
 }
 
 const serveCommand = argv => {
   const app = require('./serve.js');
   const port = argv.port;
-  app.listen(port, () => {
-    const url = chalk.underline(`http://localhost:${port}`);
+  const host = argv.host;
+  app.listen(port, host, () => {
+    const url = chalk.underline(`http://${host}:${port}`);
     const message = chalk.keyword('green').bold(`Webdash running on port ${url}!`);
     console.log(message);
   });
 };
 
 require('yargs') // eslint-disable-line
-.command('serve [port]', 'start webdash server', serveOptions, serveCommand)
+.command('serve [--port] [--host]', 'start webdash server', serveOptions, serveCommand)
 .command('$0', 'the default command', serveOptions, serveCommand)
 .argv
