397f2ca85ec095ed981328864d34414d6a147690
Jad Joubran <joubran.jad@gmail.com>
2018-04-01 17:03:07 +0300

    Monkey testing :(

--- scripts/app-root.js
+++ scripts/app-root.js
@@ -1,5 +1,6 @@
 const isGlobal = require("is-global");
 let appRoot = require("app-root-path").toString();
+console.log({appRoot});
 
 //if webdash is running as global
 //gotcha: isGlobal will return true when running `npm install --save-dev webdash`
