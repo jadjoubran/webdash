a75fbfcc336936978fec848b8051ea978ed360ea
Jad Joubran <joubran.jad@gmail.com>
2018-04-01 16:24:53 +0300

    Add console logs

--- scripts/app-root.js
+++ scripts/app-root.js
@@ -1,14 +1,16 @@
 const isGlobal = require("is-global");
 let appRoot = require("app-root-path").toString();
-
+console.log({appRoot});
 //if webdash is running as global
 if (isGlobal()){
+  console.log('is global');
   appRoot = process.cwd();
+  console.log({appRoot});
 }
 
 let npmLinkMode = process.argv.find(arg => arg.startsWith("--project="));
 if (npmLinkMode) {
   appRoot = npmLinkMode.substr("--project=".length);
 }
-
+console.log({appRoot});
 module.exports = appRoot;
